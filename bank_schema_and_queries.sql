9b9261d5f24ac4215272cf3f1895b484d44fe9ac
aakibmujawar19 <126558270+aakibmujawar19@users.noreply.github.com>
2025-07-15 10:58:00 +0530

    Update bank_schema_and_queries.sql

--- bank_schema_and_queries.sql
+++ bank_schema_and_queries.sql
@@ -79,4 +79,37 @@ CREATE TABLE CARDS (
     FOREIGN KEY (ACCOUNT_ID) REFERENCES ACCOUNTS(ACCOUNT_ID)
 );
 
+-- DATA INSERT
+INSERT INTO CUSTOMERS VALUES (1, 'Sneha Deshmukh', '210 Shivaji Nagar, Nagpur', 918866554433, 'sneha.deshmukh@example.com');
+INSERT INTO CUSTOMERS VALUES (2, 'Irfan Shaikh', '66 Hussain Ganj, Mumbai', 919911223344, 'irfan.shaikh@example.com');
+INSERT INTO CUSTOMERS VALUES (3, 'Neha Bhat', '65 Residency Road, Bhopal', 919812344556, 'neha.bhat@example.com');
+INSERT INTO CUSTOMERS VALUES (4, 'Bilal Hashmi', '101 Hazratganj, Lucknow', 919844556677, 'bilal.hashmi@example.com');
+INSERT INTO CUSTOMERS VALUES (5, 'Preeti Kulkarni', '15 FC Road, Pune', 919900223344, 'preeti.kulkarni@example.com');
+INSERT INTO CUSTOMERS VALUES (6, 'Zoya Sheikh', '109 Park Lane, Bhopal', 918888888888, 'zoya.sheikh@example.com');
+INSERT INTO CUSTOMERS VALUES (7, 'Mary Fernandes', '33 Church Street, Kochi', 919855667788, 'mary.fernandes@example.com');
+INSERT INTO CUSTOMERS VALUES (8, 'Khalid Usmani', '23 Lal Chowk, Srinagar', 918822334455, 'khalid.usmani@example.com');
+INSERT INTO CUSTOMERS VALUES (9, 'Rajat Agarwal', '35 Park Street, Kolkata', 918877665544, 'rajat.agarwal@example.com');
+INSERT INTO CUSTOMERS VALUES (10, 'Amanpreet Singh', '17 Rajpura Road, Amritsar', 918800334455, 'amanpreet.singh@example.com');
+INSERT INTO CUSTOMERS VALUES (11, 'Adnan Farooqui', '55 Lal Bagh, Indore', 919988776655, 'adnan.farooqui@example.com');
+INSERT INTO CUSTOMERS VALUES (12, 'Rajesh Sharma', '12 Nehru Nagar, Delhi', 919812300001, 'rajesh.sharma@example.com');
+INSERT INTO CUSTOMERS VALUES (13, 'Parveen Alam', '89 Garden Road, Patna', 918899445566, 'parveen.alam@example.com');
+INSERT INTO CUSTOMERS VALUES (16, 'Mahira Khan', '4 Charbagh, Lucknow', 919877223311, 'mahira.khan@example.com');
+INSERT INTO CUSTOMERS VALUES (18, 'Nazia Sultana', '24 Moti Mahal Rd, Lucknow', 917700556677, 'nazia.sultana@example.com');
+INSERT INTO CUSTOMERS VALUES (21, 'Vijay Shetty', '12 MG Road, Mangalore', 919812300002, 'vijay.shetty@example.com');
+INSERT INTO CUSTOMERS VALUES (22, 'Suresh Patel', '201 Gandhi Marg, Ahmedabad', 917733221100, 'suresh.patel@example.com');
+INSERT INTO CUSTOMERS VALUES (23, 'Simran Kaur', '22 Sector 15, Chandigarh', 918899998877, 'simran.kaur@example.com');
+INSERT INTO CUSTOMERS VALUES (24, 'Salman Baig', '19 Red Hills, Hyderabad', 919811112222, 'salman.baig@example.com');
+INSERT INTO CUSTOMERS VALUES (28, 'Joseph D’Souza', '9 Beach Road, Goa', 918800112233, 'joseph.dsouza@example.com');
+INSERT INTO CUSTOMERS VALUES (29, 'Lata Iyer', '20 Brigade Road, Bengaluru', 919811443322, 'lata.iyer@example.com');
+INSERT INTO CUSTOMERS VALUES (30, 'Ayesha Khan', '12 MG Road, Lucknow', 919812345678, 'ayesha.khan@example.com');
+INSERT INTO CUSTOMERS VALUES (31, 'Sana Parveen', '7 Golconda Fort Rd, Hyderabad', 919977665544, 'sana.parveen@example.com');
+INSERT INTO CUSTOMERS VALUES (33, 'Gitanjali Joshi', '99 Lakshmi Road, Pune', 919811334455, 'gitanjali.joshi@example.com');
+INSERT INTO CUSTOMERS VALUES (34, 'Rafiq Qadri', '82 Chowk Bazaar, Ahmedabad', 919988112233, 'rafiq.qadri@example.com');
+INSERT INTO CUSTOMERS VALUES (37, 'Manpreet Kaur', '101 Sector 35, Chandigarh', 919988123456, 'manpreet.kaur@example.com');
+INSERT INTO CUSTOMERS VALUES (38, 'Zahra Mirza', '5 Dal Lake View, Srinagar', 919922334455, 'zahra.mirza@example.com');
+INSERT INTO CUSTOMERS VALUES (40, 'Arvind Pillai', '45 Anna Salai, Chennai', 919811998877, 'arvind.pillai@example.com');
+INSERT INTO CUSTOMERS VALUES (42, 'Imran Ansari', '45 Civil Lines, Jaipur', 919876543210, 'imran.ansari@example.com');
+INSERT INTO CUSTOMERS VALUES (43, 'Kavita Verma', '42 Sector 14, Noida', 919811223344, 'kavita.verma@example.com');
+INSERT INTO CUSTOMERS VALUES (47, 'Sanjay Menon', '6 MG Road, Ernakulam', 918812334455, 'sanjay.menon@example.com');
+INSERT INTO CUSTOMERS VALUES (49, 'Farida Fernandes', '55 Chapel Road, Mumbai', 919988445566, 'farida.fernandes@example.com');
 
